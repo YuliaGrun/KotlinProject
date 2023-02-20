0f3ecbe8792d0ac63efd1c6dc8c418bf8fe237d1
YuliaGrun <51439701+YuliaGrun@users.noreply.github.com>
2023-02-20 10:55:45 +0300

    button click, data class
    Добавить кнопку в разметку и повесить на неё clickListener в Activity.
    Сформировать data class с двумя свойствами и вывести их на экран приложения.

--- app/src/main/java/com/example/kotlinproject/MainActivity.kt
+++ app/src/main/java/com/example/kotlinproject/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.kotlinproject
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlinproject.domain.Film
 
 class MainActivity : AppCompatActivity() {
+
+    private val film: Film = Film("Harry Potter: The Complete Series", 2001)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun onButtonClick(view: View) {
+        Toast.makeText(this, R.string.testClick,Toast.LENGTH_SHORT).show()
+    }
+
+    fun onNameButton(view: View) {
+        Toast.makeText(this,film.name,Toast.LENGTH_SHORT).show()
+    }
+    fun onYearButton(view: View) {
+        Toast.makeText(this,film.year,Toast.LENGTH_SHORT).show()
     }
 }
